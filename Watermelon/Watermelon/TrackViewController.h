c16e448c34d762725b423b07488d529aa6e7cc94
Ian Chen <ian@Ians-MacBook-Pro.local>
2015-04-07 17:32:25 -0700

    new view controller

---
+++ Watermelon/Watermelon/TrackViewController.h
@@ -0,0 +1,14 @@
+//
+//  TrackViewController.h
+//  Watermelon
+//
+//  Created by Ian Chen on 7/4/15.
+//  Copyright (c) 2015 Ian Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrackViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *chartImgView;
+
+@end
---
+++ Watermelon/Watermelon/TrackViewController.m
@@ -0,0 +1,37 @@
+//
+//  TrackViewController.m
+//  Watermelon
+//
+//  Created by Ian Chen on 7/4/15.
+//  Copyright (c) 2015 Ian Chen. All rights reserved.
+//
+
+#import "TrackViewController.h"
+
+@interface TrackViewController ()
+
+@end
+
+@implementation TrackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
